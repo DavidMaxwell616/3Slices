7a4a08b8b3e35f3ac676971ff8d005d8aa712ded
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2019-09-23 21:36:19 -0700

    update

--- js/gameTemplate.js
+++ js/gameTemplate.js
@@ -28,9 +28,30 @@ class playGame extends Phaser.Scene {
     super("PlayGame");
   }
   create() {
+    let reverse = -1;
     this.matter.world.update30Hz();
-    this.matter.world.setBounds(10, 10, game.config.width - 20, game.config.height - 20);
-    this.matter.add.rectangle(game.config.width / 2 - 50, game.config.width / 2, 100, 300);
+    this.cameras.main.setBackgroundColor(0xCCCCCC);
+    this.matter.world.setBounds(10, 10, game.config.width - 10, game.config.height - 10);
+    var path = '0.00, 2.00, 172.00, 0.00, 180.00, 304.00, 0.00, 296.00';
+    var verts = this.matter.verts.fromPath(path);
+    for (let i = 0; i < verts.length; i++) {
+      verts[i].x *= reverse;
+      verts[i].y *= reverse;
+    }
+    console.log(verts);
+    var poly = this.add.polygon(361.033, 366.5909, verts, 0xff0000);
+    //console.log(poly);
+    poly.setStrokeStyle(2, 0x00);
+    let polyCentre = Phaser.Physics.Matter.Matter.Vertices.centre(verts)
+    this.matter.add.gameObject(poly, {
+      shape: {
+        type: 'fromVerts',
+        verts,
+        flagInternal: true
+      }
+    }).setOrigin(0.5 * reverse, 0.5 * reverse);
+
+    //   this.matter.add.rectangle(game.config.width / 2 - 50, game.config.width / 2, 100, 300);
     this.lineGraphics = this.add.graphics();
     this.input.on("pointerdown", this.startDrawing, this);
     this.input.on("pointerup", this.stopDrawing, this);
@@ -43,7 +64,7 @@ class playGame extends Phaser.Scene {
   keepDrawing(pointer) {
     if (this.isDrawing) {
       this.lineGraphics.clear();
-      this.lineGraphics.lineStyle(1, 0x00ff00);
+      this.lineGraphics.lineStyle(2, 0x000000);
       this.lineGraphics.moveTo(pointer.downX, pointer.downY);
       this.lineGraphics.lineTo(pointer.x, pointer.y);
       this.lineGraphics.strokePath();
@@ -71,7 +92,9 @@ class playGame extends Phaser.Scene {
       }
     }
     toBeSliced.forEach(function (body) {
-      this.matter.world.remove(body)
+      var dynamic = !body.gameObject.isStatic;
+      body.gameObject.destroy();
+      this.matter.world.remove(body);
     }.bind(this))
     toBeCreated.forEach(function (points) {
       let polyObject = [];
