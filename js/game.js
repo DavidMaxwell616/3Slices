4a0fa9d3f1066d35142a958222a34458490bf558
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2020-03-26 22:36:06 -0700

    preloader

--- js/game.js
+++ js/game.js
@@ -80,6 +80,7 @@ class playGame extends Phaser.Scene {
   }
 
   preload() {
+    this.showLoader(this);
     this.load.json('levelData', 'assets/json/map1.json');
     this.load.image('do_over', 'assets/images/do_over.png');
     this.load.image('go_back', 'assets/images/go_back.png');
@@ -88,6 +89,53 @@ class playGame extends Phaser.Scene {
     this.load.image('title', 'assets/images/title.png');
     this.load.image('titleShadow', 'assets/images/titleShadow.png');
     this.load.image('maxxdaddy', 'assets/images/maxxdaddy.gif');
+  }
+
+  showLoader(game) {
+    var progressBar = game.add.graphics();
+    var progressBox = game.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(340, 270, 320, 50);
+    game.load.on('progress', function (value) {});
+
+    game.load.on('fileprogress', function (file) {});
+
+    game.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+    });
+    game.load.on('progress', function (value) {
+      progressBar.clear();
+      progressBar.fillStyle(0xff4500, 1);
+      progressBar.fillRect(350, 280, 300 * value, 30);
+      percentText.setText(parseInt(value * 100) + '%');
+    });
+
+    var width = game.cameras.main.width;
+    var height = game.cameras.main.height;
+    var loadingText = game.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = game.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
   }
 
   create() {
